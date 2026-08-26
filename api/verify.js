89d711a1cb9f78804077f1a7be7431f76cda698f
yaassenahmed2021-del <yaassenahmed2021@gmail.com>
2026-08-26 22:28:42 +0300

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -9,7 +9,7 @@ import { kv } from '@vercel/kv';
  *   1) TFS-HR4-A7K9M2X4Q8W1
  *   2) TFS-HR4-P3N6R8Y2L5Z0
  */
-const VALID_KEYS = ["NEWedition2026"];
+const VALID_KEYS = ["123456789"];
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
